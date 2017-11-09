aa980dc75a2e8e0a54b1214912197132581776f9
gmd1apeterbos <97040824@st.deltion.nl>
2017-11-09 08:58:04 +0100

    Big boy commit

---
+++ Prototyping/Fantasy Dungeon Keeper/Prototyping Fantasy Dungeon Keeper/Assets/Scripts/TortManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TortManager : MonoBehaviour {
+    public static bool isTorturing;
+    public static Vector3 tortPos;
+
+    public static GameObject beingTorted;
+
+    public static int infoGathered;
+    public Text infoGatherText;
+
+    public static int goalInfo;
+	// Use this for initialization
+	void Start () {
+        tortPos = GameObject.FindGameObjectWithTag("TortPos").transform.position;
+        goalInfo = 1000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        infoGatherText.text = "Info gathered: " + infoGathered;
+	}
+
+    public static void Leave()
+    {
+        Enemy curen = beingTorted.GetComponent<Enemy>();
+        beingTorted.transform.position = curen.startPos;
+        curen.StartCoroutine(curen.Recover());
+        isTorturing = false;
+        beingTorted = null;
+    }
+
+    
+}
